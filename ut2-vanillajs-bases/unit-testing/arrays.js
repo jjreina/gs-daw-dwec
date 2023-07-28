d3432ce215d5eaffa1bd6c08b4ee1b95875e8862
jjreina <jreinapascual@gmail.com>
2023-07-28 11:13:35 +0200

    dwec - added new functions and test associates

--- ut2-vanillajs-bases/unit-testing/arrays.js
+++ ut2-vanillajs-bases/unit-testing/arrays.js
@@ -31,3 +31,74 @@ export const addFirstPosition = (item) => {
 export const addLastPosition = (item) => {
   return games.push(item);
 };
+
+/******************************************************/
+export const got = [
+  "Lannister",
+  "Targaryen",
+  "Stark",
+  "Baratheon",
+  "Greyjoy",
+  "Arryn",
+  "Martell",
+];
+
+/**
+ * Removes Baratheon and Greyjoy houses
+ * @returns array with items removes
+ */
+export const removeBaratheonAndGreyjoy = () => {
+  return got.splice(3, 2);
+};
+
+/**
+ * Removes Arryn house
+ * @returns array with items removes
+ */
+export const removeArryn = () => {
+  return got.splice(3, 1);
+};
+
+/******************************************************/
+export const narutoCharacters = [
+  "Naruto",
+  "Sasuke",
+  "Sakura",
+  "Hinata",
+  "Kakashi",
+  "Jiraiya",
+  "Itachi",
+  "Gaara",
+];
+
+/**
+ * Gets the index of Naruto
+ * @returns the index found.
+ */
+export const getNaruto = () => {
+  return narutoCharacters.indexOf("Naruto");
+};
+
+/**
+ * Gets the index of Gaara
+ * @returns the index found.
+ */
+export const getGaara = () => {
+  return narutoCharacters.indexOf("Gaara");
+};
+
+/**
+ * Gets the index of Rock Lee
+ * @returns the index found.
+ */
+export const getRockLee = () => {
+  return narutoCharacters.indexOf("Rock Lee");
+};
+
+/**
+ * Reverses the elements in Naruto Characters array
+ * @returns the reversed given array
+ */
+export const reverseNarutoCharacters = () => {
+  return narutoCharacters.reverse();
+};
