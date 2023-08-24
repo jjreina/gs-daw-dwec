dfe583bfab084fd1899f17bdfc62ff15e277df9f
jjreina <jreinapascual@gmail.com>
2023-08-24 17:52:45 +0200

    dwec - added some unit tests for literals objects

--- ut2-vanillajs-bases/unit-testing/literals-objects.test.js
+++ ut2-vanillajs-bases/unit-testing/literals-objects.test.js
@@ -1,7 +1,35 @@
 import * as literals from "./literals-objects";
 
-describe("Gets", () => {
-  it("should returns Naruto index", () => {
-    expect(literals.person.getAge()).toBe(32);
+describe("Access to objects literals properties", () => {
+  it("should returns the street of person object", () => {
+    expect(literals.getStreet()).toBe("C/ Larios");
+  });
+
+  it("should returns the country of person object", () => {
+    expect(literals.getCountry()).toBe("USA");
+  });
+
+  it("should returns the age of person object", () => {
+    expect(literals.getAgeViaFuction()).toBe(32);
+  });
+
+  it("should remove the number property of person object", () => {
+    literals.removeNumbreAddressProperty();
+    expect(literals.getNumbreAddress()).toBe(undefined);
+  });
+
+  it("should adds the Spanish nacionality to person object", () => {
+    literals.addSpanishNacionalityAsNewProperty();
+    expect(literals.getNacionalty()).toBe("Spanish");
+  });
+
+  it("should returns a array of object person", () => {
+    let personArray = literals.getPersonArray();
+    expect(personArray[0][0]).toBe("name");
+    expect(personArray[1][0]).toBe("surname");
+    expect(personArray[2][0]).toBe("country");
+    expect(personArray[3][0]).toBe("age");
+    expect(personArray[4][0]).toBe("getAge");
+    expect(personArray[5][0]).toBe("address");
   });
 });
