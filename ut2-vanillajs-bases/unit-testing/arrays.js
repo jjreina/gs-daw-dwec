b53982bf5351fc787d8331e2f1acf268c42fa195
jjreina <jreinapascual@gmail.com>
2023-09-25 13:03:19 +0200

    gs - Added TODO

--- ut2-vanillajs-bases/unit-testing/arrays.js
+++ ut2-vanillajs-bases/unit-testing/arrays.js
@@ -4,25 +4,33 @@ export const games = ["Zelda", "Mario Bros", "Metroid", "Chrono"];
  * Removes the first element from an array and returns it.
  * If the array is empty, undefined is returned and the array is not modified.
  */
-export const deleteFirtPosition = () => {};
+export const deleteFirtPosition = () => {
+  // TODO: implement
+};
 
 /**
  * Removes the last element from an array and returns it.
  * If the array is empty, undefined is returned and the array is not modified.
  */
-export const deleteLastPosition = () => {};
+export const deleteLastPosition = () => {
+  // TODO: implement
+};
 
 /**
  * Inserts new element at the start of an array, and returns the new length of the array.
  * @param item Element to insert at the start of the array.
  */
-export const addFirstPosition = (item) => {};
+export const addFirstPosition = (item) => {
+  // TODO: implement
+};
 
 /**
  * Appends new element to the end of an array, and returns the new length of the array.
  * @param item New element to add to the array.
  */
-export const addLastPosition = (item) => {};
+export const addLastPosition = (item) => {
+  // TODO: implement
+};
 
 /******************************************************/
 export const got = [
@@ -39,13 +47,17 @@ export const got = [
  * Removes Baratheon and Greyjoy houses
  * @returns array with items removes
  */
-export const removeBaratheonAndGreyjoy = () => {};
+export const removeBaratheonAndGreyjoy = () => {
+  // TODO: implement
+};
 
 /**
  * Removes Arryn house
  * @returns array with items removes
  */
-export const removeArryn = () => {};
+export const removeArryn = () => {
+  // TODO: implement
+};
 
 /******************************************************/
 export const narutoCharacters = [
@@ -63,22 +75,30 @@ export const narutoCharacters = [
  * Gets the index of Naruto
  * @returns the index found.
  */
-export const getNaruto = () => {};
+export const getNaruto = () => {
+  // TODO: implement
+};
 
 /**
  * Gets the index of Gaara
  * @returns the index found.
  */
-export const getGaara = () => {};
+export const getGaara = () => {
+  // TODO: implement
+};
 
 /**
  * Gets the index of Rock Lee
  * @returns the index found.
  */
-export const getRockLee = () => {};
+export const getRockLee = () => {
+  // TODO: implement
+};
 
 /**
  * Reverses the elements in Naruto Characters array
  * @returns the reversed given array
  */
-export const reverseNarutoCharacters = () => {};
+export const reverseNarutoCharacters = () => {
+  // TODO: implement
+};
