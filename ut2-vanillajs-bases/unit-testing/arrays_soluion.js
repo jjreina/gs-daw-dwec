237648989258b89af8b19fead514d5c1f88a83bb
jjreina <jreinapascual@gmail.com>
2023-09-25 12:56:02 +0200

    gs - added test file for alumns

--- ut2-vanillajs-bases/unit-testing/arrays_soluion.js
+++ ut2-vanillajs-bases/unit-testing/arrays_soluion.js
@@ -4,25 +4,33 @@ export const games = ["Zelda", "Mario Bros", "Metroid", "Chrono"];
  * Removes the first element from an array and returns it.
  * If the array is empty, undefined is returned and the array is not modified.
  */
-export const deleteFirtPosition = () => {};
+export const deleteFirtPosition = () => {
+  return games.shift();
+};
 
 /**
  * Removes the last element from an array and returns it.
  * If the array is empty, undefined is returned and the array is not modified.
  */
-export const deleteLastPosition = () => {};
+export const deleteLastPosition = () => {
+  return games.pop();
+};
 
 /**
  * Inserts new element at the start of an array, and returns the new length of the array.
  * @param item Element to insert at the start of the array.
  */
-export const addFirstPosition = (item) => {};
+export const addFirstPosition = (item) => {
+  return games.unshift(item);
+};
 
 /**
  * Appends new element to the end of an array, and returns the new length of the array.
  * @param item New element to add to the array.
  */
-export const addLastPosition = (item) => {};
+export const addLastPosition = (item) => {
+  return games.push(item);
+};
 
 /******************************************************/
 export const got = [
@@ -39,13 +47,17 @@ export const got = [
  * Removes Baratheon and Greyjoy houses
  * @returns array with items removes
  */
-export const removeBaratheonAndGreyjoy = () => {};
+export const removeBaratheonAndGreyjoy = () => {
+  return got.splice(3, 2);
+};
 
 /**
  * Removes Arryn house
  * @returns array with items removes
  */
-export const removeArryn = () => {};
+export const removeArryn = () => {
+  return got.splice(3, 1);
+};
 
 /******************************************************/
 export const narutoCharacters = [
@@ -63,22 +75,30 @@ export const narutoCharacters = [
  * Gets the index of Naruto
  * @returns the index found.
  */
-export const getNaruto = () => {};
+export const getNaruto = () => {
+  return narutoCharacters.indexOf("Naruto");
+};
 
 /**
  * Gets the index of Gaara
  * @returns the index found.
  */
-export const getGaara = () => {};
+export const getGaara = () => {
+  return narutoCharacters.indexOf("Gaara");
+};
 
 /**
  * Gets the index of Rock Lee
  * @returns the index found.
  */
-export const getRockLee = () => {};
+export const getRockLee = () => {
+  return narutoCharacters.indexOf("Rock Lee");
+};
 
 /**
  * Reverses the elements in Naruto Characters array
  * @returns the reversed given array
  */
-export const reverseNarutoCharacters = () => {};
+export const reverseNarutoCharacters = () => {
+  return narutoCharacters.reverse();
+};
