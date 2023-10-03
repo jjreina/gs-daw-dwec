9bae90f56ef2ad0641a87a9bc4064b9220c33189
jjreina <jreinapascual@gmail.com>
2023-10-03 21:53:42 +0200

    gs - testing repleaceChildren

--- ut3-vanillajs-dom/02-creators-inserts/01/main.js
+++ ut3-vanillajs-dom/02-creators-inserts/01/main.js
@@ -3,8 +3,11 @@ const img = document.createElement("img");
 const h1 = document.createElement("h1");
 const p_1 = document.createElement("p");
 const p_2 = document.createElement("p");
+const p_3 = document.createElement("p");
+const span_1 = document.createElement("span");
+const span_2 = document.createElement("span");
 
-img.src = "./assests/img_la.jpg";
+img.src = "./assest/img_la.jpg";
 img.alt = "LA";
 
 body.appendChild(img);
@@ -17,3 +20,13 @@ body.appendChild(p_1);
 
 p_2.textContent = "This is a another paragraph.";
 body.appendChild(p_2);
+
+p_3.textContent = "This is a another paragraph.";
+body.appendChild(p_3);
+
+span_1.textContent = "New Span";
+body.appendChild(span_1);
+
+span_2.textContent = "New Span 2";
+
+body.replaceChildren(span_2);
