43246aa89dda985bd24835980f9b7f1926a157b3
jjreina <jreinapascual@gmail.com>
2023-10-05 09:58:21 +0200

    gs - Create and insert

--- ut3-vanillajs-dom/02-creators-inserts/01/main.js
+++ ut3-vanillajs-dom/02-creators-inserts/01/main.js
@@ -1,32 +1,22 @@
 const body = document.querySelector("body");
+
 const img = document.createElement("img");
 const h1 = document.createElement("h1");
 const p_1 = document.createElement("p");
 const p_2 = document.createElement("p");
 const p_3 = document.createElement("p");
-const span_1 = document.createElement("span");
-const span_2 = document.createElement("span");
 
-img.src = "./assest/img_la.jpg";
+img.src = "./assests/img_la.jpg";
 img.alt = "LA";
-
-body.appendChild(img);
-
 h1.textContent = "This is a Heading";
-body.appendChild(h1);
-
 p_1.textContent = "This is a paragraph.";
-body.appendChild(p_1);
-
 p_2.textContent = "This is a another paragraph.";
-body.appendChild(p_2);
-
 p_3.textContent = "This is a another paragraph.";
-body.appendChild(p_3);
 
-span_1.textContent = "New Span";
-body.appendChild(span_1);
+/*body.appendChild(img);
+body.appendChild(h1);
+body.appendChild(p_1);
+body.appendChild(p_2);
+body.appendChild(p_3);*/
 
-span_2.textContent = "New Span 2";
-
-body.replaceChildren(span_2);
+body.append(img, h1, p_1, p_2, p_3);
