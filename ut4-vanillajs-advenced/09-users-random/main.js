0ae0cffe69a13323c6482ca63f336500d4f73863
jjreina <jreinapascual@gmail.com>
2023-11-15 10:53:43 +0100

    Tarea evaluable Random user

--- ut4-vanillajs-advenced/09-users-random/main.js
+++ ut4-vanillajs-advenced/09-users-random/main.js
@@ -1,11 +1,26 @@
 import "./style.css";
 import { Person } from "./person";
 
-let person = new Person();
-let info = "";
+const url_radomuser_api = "https://randomuser.me/api/";
+const url_world_time_api =
+  "https://world-time-by-api-ninjas.p.rapidapi.com/v1/worldtime?city=";
+const world_time_options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6185a78aa2mshb04ae8991085691p1d093bjsnae8623070f4a",
+    "X-RapidAPI-Host": "world-time-by-api-ninjas.p.rapidapi.com",
+  },
+};
 const BUTTON_TEXT = "GENERATE USER";
 
 const container = document.querySelector(".container");
+const completeNameSpan = document.createElement("span");
+const mailSpan = document.createElement("span");
+const phoneSpan = document.createElement("span");
+const img = document.createElement("img");
+
+let person = new Person();
+let info = "";
 
 const card = document.createElement("div");
 card.className = "card";
@@ -16,10 +31,30 @@ button.textContent = BUTTON_TEXT;
 container.appendChild(card);
 container.appendChild(button);
 
+const createContentCard = (name, surname, mail, phone, image) => {
+  completeNameSpan.innerHTML = `<strong>Name:</strong> ${name} ${surname}`;
+  mailSpan.innerHTML = `<strong>Mail:</strong> ${mail}`;
+  phoneSpan.innerHTML = `<strong>Phone:</strong> ${phone}`;
+  img.src = image;
+  card.appendChild(img);
+  card.appendChild(completeNameSpan);
+  card.appendChild(mailSpan);
+  card.appendChild(phoneSpan);
+};
+
 button.addEventListener("click", async () => {
-  const url = "https://randomuser.me/api/";
-  const response = await fetch(url);
-  const { results } = await response.json();
-  info = results[0];
-  console.log((person.name_first = info.name.first));
+  try {
+    const response = await fetch(url_radomuser_api);
+    const { results } = await response.json();
+    info = results[0];
+    createContentCard(
+      info.name.first,
+      info.name.last,
+      info.email,
+      info.phone,
+      info.picture.large
+    );
+  } catch (error) {
+    console.log(new Error(error));
+  }
 });
