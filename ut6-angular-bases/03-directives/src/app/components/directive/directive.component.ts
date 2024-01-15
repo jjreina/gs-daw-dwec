1a5bf3ec56b698db623df94fd77db186ab695b21
jjreina <jreinapascual@gmail.com>
2024-01-15 13:16:30 +0100

    Angular - else

--- ut6-angular-bases/03-directives/src/app/components/directive/directive.component.ts
+++ ut6-angular-bases/03-directives/src/app/components/directive/directive.component.ts
@@ -7,9 +7,16 @@ import { Component } from '@angular/core';
 })
 export class DirectiveComponent {
   public title: string = 'Directive IF Component';
+  public title2: string = 'Directive Else Component';
   public directiveIfModern: string = 'Working with @if';
   public showNgIfText: boolean = false;
   public showIfText: boolean = false;
+  public loginOld: boolean = false;
+  public loginNew: boolean = false;
+  public nameOld: string | undefined = '';
+  public surnameOld: string | undefined = '';
+  public name: string | undefined = '';
+  public surname: string | undefined = '';
 
   public mouseOverParagraphNgIf(): void {
     this.showNgIfText = !this.showNgIfText;
@@ -19,5 +26,18 @@ export class DirectiveComponent {
   public mouseOverParagraphIf(): void {
     this.showNgIfText = false;
     this.showIfText = !this.showIfText;
+  }
+
+  public logInOld(): void {
+    this.loginOld = true;
+    this.nameOld = document.querySelector<HTMLInputElement>('#name')?.value;
+    this.surnameOld =
+      document.querySelector<HTMLInputElement>('#surname')?.value;
+  }
+
+  public logInNew(): void {
+    this.loginNew = true;
+    this.name = document.querySelector<HTMLInputElement>('#name')?.value;
+    this.surname = document.querySelector<HTMLInputElement>('#surname')?.value;
   }
 }
