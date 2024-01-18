406435838839f989f334a884102ccfd82e7b7a0e
jjreina <jreinapascual@gmail.com>
2024-01-18 10:53:40 +0100

    Angular - app movies by for directive

---
+++ ut6-angular-bases/05-for/src/app/components/movie/movie.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components-movie',
+  templateUrl: './movie.component.html',
+  styleUrl: './movie.component.css',
+})
+export class MovieComponent {}
