21e1f68ad63314f4fcff6b2d68e5875e17b4bfd0
jjreina <jreinapascual@gmail.com>
2024-01-20 21:46:35 +0100

    Angular - for control flow example

--- ut6-angular-bases/05-for/src/app/app.module.ts
+++ ut6-angular-bases/05-for/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MoviesComponent } from './components/movies/movies.component';
-import { MovieComponent } from './components/movie/movie.component';
 import { ForComponent } from './components/for/for.component';
 
 @NgModule({
-  declarations: [AppComponent, MoviesComponent, MovieComponent, ForComponent],
+  declarations: [AppComponent, ForComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- ut6-angular-bases/05-for/src/app/components/movie/movie.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-components-movie',
-  templateUrl: './movie.component.html',
-  styleUrl: './movie.component.css',
-})
-export class MovieComponent {}
