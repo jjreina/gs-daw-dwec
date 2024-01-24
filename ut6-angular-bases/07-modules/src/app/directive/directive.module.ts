88f7602182e283c476c892ae0f205b5a1ac96a0d
jjreina <jreinapascual@gmail.com>
2024-01-24 20:21:53 +0100

    Angular - proyect refactor

--- ut6-angular-bases/07-modules/src/app/directive/directive.module.ts
+++ ut6-angular-bases/07-modules/src/app/directive/directive.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { DirectiveComponent } from './components/directive/directive.component';
 import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './pages/main-page.component';
 
 @NgModule({
-  declarations: [DirectiveComponent],
-  exports: [DirectiveComponent],
+  declarations: [DirectiveComponent, MainPageComponent],
+  exports: [MainPageComponent],
   imports: [CommonModule],
 })
 export class DirectiveModule {}
