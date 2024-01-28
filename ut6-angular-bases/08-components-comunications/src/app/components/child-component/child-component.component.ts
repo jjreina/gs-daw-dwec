9b6c99e28a71fc7db1328bd6d8e8ab60c7bfdfcc
jjreina <jreinapascual@gmail.com>
2024-01-28 19:30:40 +0100

    Angular - output decorator

--- ut6-angular-bases/08-components-comunications/src/app/components/child-component/child-component.component.ts
+++ ut6-angular-bases/08-components-comunications/src/app/components/child-component/child-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child-component',
@@ -8,4 +8,11 @@ import { Component, Input } from '@angular/core';
 export class ChildComponentComponent {
   @Input()
   public item: string = '';
+
+  @Output()
+  public newItemEvent: EventEmitter<string> = new EventEmitter<string>();
+
+  public addNewItem(value: string): void {
+    this.newItemEvent.emit(value);
+  }
 }
