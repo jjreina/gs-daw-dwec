92c75f653a4cd2dc30640d0700e29240758b760c
jjreina <jreinapascual@gmail.com>
2024-01-28 21:12:41 +0100

    Angular - App Basic Movier Added Output decarator

--- ut6-angular-bases/06-movies/src/app/movies/components/movie/movie.component.ts
+++ ut6-angular-bases/06-movies/src/app/movies/components/movie/movie.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Movie } from '../../interfaces/movieInterface';
 
 @Component({
@@ -9,4 +9,12 @@ import { Movie } from '../../interfaces/movieInterface';
 export class MovieComponent {
   @Input()
   public movie!: Movie;
+
+  @Output()
+  public movieInfoEventEmitter: EventEmitter<string> =
+    new EventEmitter<string>();
+
+  public emitMovieTitle(title: string): void {
+    this.movieInfoEventEmitter.emit(title);
+  }
 }
